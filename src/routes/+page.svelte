bc3d377ad23ac64d5beb3e677a768f8c393c8782
RobinJahn <robin@kamerapilot.com>
2023-06-17 17:44:47 +0200

    Added Landing Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<div class="notification is-dark">
-    Welcome to Your Photo Map!
+<script>
+    import Header from '$lib/Header.svelte';
+</script>
+
+<Header />
+
+<div class="columns is-vcentered content">
+    <div class="column has-text-centered">
+        <h1 class="title">GlobeLens: Explore Your Journey Through Your Lens</h1>
+        <p>Map Your Memories. Relive Your Adventures. Inspire New Ones.</p>
+    </div>
 </div>
