1c2f0a7920b7217336c87abda7799e9616b4dfd2
RobinJahn <robin@kamerapilot.com>
2023-06-19 11:15:58 +0200

    Added /map, shows leaflet map

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import MainNavigator from "$lib/MainNavigator.svelte";
+    import PlacemarkMap from "$lib/PlacemarkMap.svelte";
+
+</script>
+
+<Header>
+    <MainNavigator />
+</Header>
+
+<PlacemarkMap />
