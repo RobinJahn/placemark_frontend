604379ed1740e5ccbd58c2a00c8c0e16a3d8e031
RobinJahn <robin@kamerapilot.com>
2023-06-23 21:05:01 +0200

    Improved reload of site elements at switch of placemark at /poi/[id]

--- src/routes/poi/[id]/+page.svelte
+++ src/routes/poi/[id]/+page.svelte
@@ -5,7 +5,6 @@
     import {placemarkService} from "../../../services/placemark-service.js";
     import MainNavigator from "$lib/MainNavigator.svelte";
     import Header from "$lib/Header.svelte";
-    import {each} from "svelte/internal";
 
     let id;
     let placemark;
@@ -69,80 +68,75 @@
 
 <div class="card box m-2">
     {#if placemark && user}
-        <header class="card-header">
-            <p class="card-header-title is-size-3 has-text-centered is-centered" contenteditable={isEditable}
-               on:input={updateTitleValue}>
-                {titleText}
-            </p>
+        {#each [placemark] as p (p)}
+            <header class="card-header">
+                <p class="card-header-title is-size-3 has-text-centered is-centered" contenteditable={isEditable}
+                   on:input={updateTitleValue}>
+                    {titleText}
+                </p>
 
-            <button class="button my-auto mr-3" on:click={toggleEditable}>
-                {#if isEditable}
-                    <i class="fas fa-save fa-lg"></i>
-                {:else}
-                    <i class="fas fa-edit fa-lg"></i>
-                {/if}
-            </button>
-        </header>
+                <button class="button my-auto mr-3" on:click={toggleEditable}>
+                    {#if isEditable}
+                        <i class="fas fa-save fa-lg"></i>
+                    {:else}
+                        <i class="fas fa-edit fa-lg"></i>
+                    {/if}
+                </button>
+            </header>
 
-        <div class="card-content">
+            <div class="card-content">
 
-            <div class="content">
-                <div class="columns">
-                    <div class="column">
-                        {#each [placemark] as p (p)}
+                <div class="content">
+                    <div class="columns">
+                        <div class="column">
                             <PlacemarkMap id="street-map" marker={p} setAll={false} zoom={15} height={50}/>
-                        {/each}
+                        </div>
+                        <div class="column is-half">
+                            <PlacemarkMap id="satellite-map" marker={p} setAll={false} zoom={15} height={50} showLayer="Satellite"/>
+                        </div>
                     </div>
-                    <div class="column is-half">
-                        {#each [placemark] as p (p)}
-                            <PlacemarkMap id="satellite-map" marker={p} setAll={false} zoom={15} height={50}
-                                          showLayer="Satellite"/>
-                        {/each}
-                    </div>
-                </div>
-                <div class="columns">
-                    <div class="column is-half">
-                        {#each [placemark] as p (p)}
+                    <div class="columns">
+                        <div class="column is-half">
                             <PlacemarkMap id="all-marker-map" setAll={true} height={50} location={placemarkLocation}/>
-                        {/each}
-                    </div>
-                    <div class="column">
-                        <p contenteditable={isEditable} on:input={updateDescriptionValue}> {descriptionText} </p>
-                        <br/>
-                        <table class="table is-fullwidth">
-                            <tbody>
-                            <tr>
-                                <td>Owner</td>
-                                <td>{user.firstName + user.lastName}</td>
-                            </tr>
-                            <tr>
-                                <td>e-Mail</td>
-                                <td>{user.email}</td>
-                            </tr>
-                            </tbody>
-                        </table>
+                        </div>
+                        <div class="column">
+                            <p contenteditable={isEditable} on:input={updateDescriptionValue}> {descriptionText} </p>
+                            <br/>
+                            <table class="table is-fullwidth">
+                                <tbody>
+                                <tr>
+                                    <td>Owner</td>
+                                    <td>{user.firstName + user.lastName}</td>
+                                </tr>
+                                <tr>
+                                    <td>e-Mail</td>
+                                    <td>{user.email}</td>
+                                </tr>
+                                </tbody>
+                            </table>
 
-                        <!-- for each placemark.img as img -->
-                        {#each placemark.image_list as img}
-                            <figure class="image is-256x256">
-                                <img src={img} alt="">
-                            </figure>
-                        {/each}
+                            <!-- for each placemark.img as img -->
+                            {#each placemark.image_list as img}
+                                <figure class="image is-256x256">
+                                    <img src={img} alt="">
+                                </figure>
+                            {/each}
 
-                        <!--figure class="image is-256x256">
-                            <img src={placemark.image_list?.[0]} alt="">
-                        </figure-->
+                            <!--figure class="image is-256x256">
+                                <img src={placemark.image_list?.[0]} alt="">
+                            </figure-->
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    {:else}
-        <div class="card-content">
-            <div class="content">
-                <p class="has-text-centered is-size-3">Loading...</p>
+        {:else}
+            <div class="card-content">
+                <div class="content">
+                    <p class="has-text-centered is-size-3">Loading...</p>
+                </div>
             </div>
-        </div>
+        {/each}
     {/if}
 </div>
